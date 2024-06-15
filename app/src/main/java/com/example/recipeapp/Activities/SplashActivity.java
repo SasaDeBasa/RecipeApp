44ef9d127fa00870cc0271e6230d1eb9c0d9b29e
Sasadara Ekanayake <99122578+SasaDeBasa@users.noreply.github.com>
2024-06-16 00:08:17 +0530

    layouts home commit

--- app/src/main/java/com/example/recipeapp/Activities/SplashActivity.java
+++ app/src/main/java/com/example/recipeapp/Activities/SplashActivity.java
@@ -1,4 +1,34 @@
 package com.example.recipeapp.Activities;
 
-public class SplashActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.recipeapp.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
 }
